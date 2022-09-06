820dd05dc7bc253e75ecb14c6d3f59039389ec60
Brad <bradlemancik@gmail.com>
2022-09-06 10:22:11 -0500

    Stuff

--- ZOOTER Hints/MainWindow.xaml.cs
+++ ZOOTER Hints/MainWindow.xaml.cs
@@ -34,6 +34,8 @@ namespace ZOOTR_Hints
         private List<String> prizes;
         private List<String> rewards;
         private List<String> exclude;
+        private string but = "";
+        private Dictionary<string, List<String>> TriItems = new Dictionary<string, List<String>>();
 
         public List<String> Locations
         {
@@ -92,9 +94,12 @@ namespace ZOOTR_Hints
 
             prizes = new List<String> { "Light", "Fire", "Water", "Shadow", "Spirit", "Stone" };
 
-            songChecks = new List<String> { "Minuet", "Burning Kak", "Prelude", "Crater",
-                "Ice Cavern", "Royal Tomb Song", "Requiem", "OoT" };
-        
+            songChecks = new List<String> { "Minuet", "Prelude", "Crater", "Ice Cavern", 
+                "Royal Tomb Song", "Requiem", "OoT" };
+
+            TriItems.Add("Wisdom", new List<String>());
+            TriItems.Add("Power", new List<String>());
+            TriItems.Add("Courage", new List<String>());
 
             locations = GetList("Locations");
             items = GetList("Items");
@@ -108,26 +113,38 @@ namespace ZOOTR_Hints
 
         private void Clear_Click(object sender, RoutedEventArgs e)
         {
+            TriItems["Wisdom"].Clear();
+            TriItems["Power"].Clear();
+            TriItems["Courage"].Clear();
+            
             foreach (object ctl in g.Children)
             {
                 if (ctl.GetType() == typeof(CheckBox))
                     ((CheckBox)ctl).IsChecked = false;
+
                 if (ctl.GetType() == typeof(TextBox))
                     ((TextBox)ctl).Text = String.Empty;
+                    
                 if (ctl.GetType() == typeof(ListBox) || ctl.GetType() == typeof(ListView))
                     ((ListBox)ctl).Items.Clear();
             }
+
+            updateStepCount("Reset");
         }
 
         private void WOTH_KeyDown(object sender, KeyEventArgs e)
         {
             if (e.Key != Key.Enter) return;
 
-            //e.Handled = true;
-            if(locations.Contains(WOTH.Text, StringComparer.InvariantCultureIgnoreCase))
+
+            if (locations.Contains(WOTH.Text, StringComparer.InvariantCultureIgnoreCase))
             {
                 woth_listBox.Items.Add(WOTH.Text);
                 WOTH.Clear();
+            } else if (locations.Contains(Foolish.Text, StringComparer.InvariantCultureIgnoreCase))
+            {
+                fool_listBox.Items.Add(Foolish.Text);
+                Foolish.Clear();
             }
         }
 
@@ -135,21 +152,67 @@ namespace ZOOTR_Hints
         {
             if (e.Key != Key.Enter) return;
 
-            //e.Handled = true;
-            string t = Found.Text;
-            if (rewards.Contains(t, StringComparer.InvariantCultureIgnoreCase))
+            TextBox s = (TextBox)sender;
+            string t = "";
+            if (s.Name.Equals("Found"))
             {
-                found_listbox.Items.Add(t);
-                if (VisualTreeHelper.GetChildrenCount(found_listbox) > 0)
+                t = Found.Text;
+                if (rewards.Contains(t, StringComparer.InvariantCultureIgnoreCase))
                 {
-                    Border border = (Border)VisualTreeHelper.GetChild(found_listbox, 0);
-                    ScrollViewer scrollViewer = (ScrollViewer)VisualTreeHelper.GetChild(border, 0);
-                    scrollViewer.ScrollToBottom();
+                    found_listbox.Items.Add(t);
+                    ScrollDown(found_listbox);
+                    Found.Clear();
                 }
-                Found.Clear();
+            } 
+            else if (s.Name.Equals("Found_Path"))
+            {
+                t = Found_Path.Text;
+                if (rewards.Contains(t, StringComparer.InvariantCultureIgnoreCase))
+                {
+                    if (pathView.SelectedIndex != -1)
+                    {
+                        Path path = (Path)pathView.Items.GetItemAt(pathView.SelectedIndex);
+
+                        path.items.Add(t);
+                        TriItems[path.triforce].Add(t);
+                        found_listbox_path.Items.Add(t);
+                        updateStepCount(path.triforce);
+
+                        foreach (Path p in pathView.Items)
+                        {
+                            if (p.region.Equals(path.region) && !p.Equals(path))
+                            {
+                                p.items.Add(t);
+                                TriItems[p.triforce].Add(t);
+                                updateStepCount(p.triforce);
+                            }
+                        }
+
+                        ScrollDown(found_listbox_path);
+                        Found_Path.Clear();
+                    }
+                    else if (but.Equals("Power") || but.Equals("Wisdom") || but.Equals("Courage"))
+                    {
+                        TriItems[but].Add(t);
+                        found_listbox_path.Items.Add(t);
+                        updateStepCount(but);
+
+                        ScrollDown(found_listbox_path);
+                        Found_Path.Clear();
+                    }
+                }
             }
         }
 
+        private void ScrollDown(ListBox found)
+        {
+            if (VisualTreeHelper.GetChildrenCount(found) > 0)
+            {
+                Border border = (Border)VisualTreeHelper.GetChild(found, 0);
+                ScrollViewer scrollViewer = (ScrollViewer)VisualTreeHelper.GetChild(border, 0);
+                scrollViewer.ScrollToBottom();
+            }
+        }
         private void All_TextChanged(object sender, TextChangedEventArgs e)
         {
             if(sender is TextBox)
@@ -173,6 +236,41 @@ namespace ZOOTR_Hints
                 if (temp.SelectedIndex == -1) return;
                 else
                 {
+                    if (temp.Name.Equals("found_listbox_path"))
+                    {
+                        if (pathView.SelectedIndex != -1)
+                        {
+                            Path path = (Path)pathView.Items.GetItemAt(pathView.SelectedIndex);
+                            string item = (string)temp.Items.GetItemAt(temp.SelectedIndex);
+                            path.items.Remove(item);
+                            TriItems[path.triforce].Remove(item);
+                            updateStepCount(path.triforce);
+                        }
+                        else if (but.Equals("Power") || but.Equals("Wisdom") || but.Equals("Courage"))
+                        {
+                            string item = (string)temp.Items.GetItemAt(temp.SelectedIndex);
+                            //foreach (Path p in pathView.Items)
+                            //{
+                            //    if (p.triforce.Equals(but) && p.items.Contains(item))
+                            //    {
+                            //        p.items.Remove(item);
+                            //        break;
+                            //    }
+                            //}
+                            TriItems[but].Remove(item);
+                            updateStepCount(but);
+                        }
+                    }
+
+                    if (temp.Name.Equals("pathView"))
+                    {
+                        Path path = (Path)temp.Items.GetItemAt(temp.SelectedIndex);
+                        List<string> found = path.items;
+                        TriItems[path.triforce].RemoveAll(t => found.Contains(t));
+                        found.Clear();
+                        updateStepCount(path.triforce);
+                    }
+
                     temp.Items.RemoveAt(temp.SelectedIndex);
                 }
             }
@@ -215,7 +313,6 @@ namespace ZOOTR_Hints
 
         private List<String> GetList(string name)
         {
-            
             string path = dir + "\\" + name + ".txt";
             List<String> gotten = new List<string>();
             if (File.Exists(path))
@@ -242,11 +339,77 @@ namespace ZOOTR_Hints
             System.Diagnostics.Process.Start(dir);
         }
 
+        private void Triforce_Click(object sender, RoutedEventArgs e)
+        {
+            Button b = sender as Button;
+            
+            switch (b.Content)
+            {
+                case ("PWR"):
+                    but = "Power";
+                    break;
+                case ("WIS"):
+                    but = "Wisdom";
+                    break;
+                case ("CRG"):
+                    but = "Courage";
+                    break;
+                default:
+                    break;
+            }
+
+            if (locations.Contains(Area.Text, StringComparer.InvariantCultureIgnoreCase))
+            {
+                Path path = new Path(Area.Text, but);
+
+                if (pathView.Items.Contains(path))
+                    return;
+
+                foreach (Path p in pathView.Items)
+                {
+                    if (p.region.Equals(Area.Text))
+                    {
+                        path.items.AddRange(p.items);
+                        TriItems[but].AddRange(p.items);
+                        updateStepCount(but);
+                    }
+                }
+
+                pathView.Items.Add(path);
+                Area.Clear();
+
+                pathView.SelectedIndex = pathView.Items.Count - 1;
+
+                Found_Path.Focus();
+
+            } 
+            else if (Area.Text.Equals(""))
+            {
+                if (found_listbox_path.SelectedIndex != -1)
+                {
+                    string item = (string)found_listbox_path.Items.GetItemAt(found_listbox_path.SelectedIndex);
+                    TriItems[but].Add(item);
+                    found_listbox_path.SelectedIndex = -1;
+                    Triforce_Click(sender, e);
+                }
+                else
+                {
+                    pathView.SelectedIndex = -1;
+                    found_listbox_path.Items.Clear();
+                    for (int i = 0; i < TriItems[but].Count; i++)
+                    {
+                        found_listbox_path.Items.Add(TriItems[but][i]);
+                    }
+                }
+            }
+            
+        }
+
         private void Check_In(object sender, KeyEventArgs e)
         {
             if (e.Key == Key.Enter)
             {
-                TextBox s = e.Source as TextBox;
+                TextBox s = sender as TextBox;
                 if (s.Name.Equals("has"))
                 {
                     if (has.Text.Trim() != "" &&
@@ -271,6 +434,48 @@ namespace ZOOTR_Hints
                         s.MoveFocus(new TraversalRequest(FocusNavigationDirection.Previous));
                     }
                 }
+            }
+        }
+
+        private void PathView_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            found_listbox_path.Items.Clear();
+            if (pathView.SelectedIndex == -1) return;
+
+            but = "";
+            Path path = (Path)pathView.Items.GetItemAt(pathView.SelectedIndex);
+            for (int i = 0; i < path.items.Count; i++)
+            {
+                found_listbox_path.Items.Add(path.items[i]);
+            }
+
+        }
+
+        private void NumTest(object sender, TextChangedEventArgs e)
+        {
+            Regex nums = new Regex(@"[^\d]");
+            TextBox temp = (TextBox)sender;
+            temp.Text = nums.Replace(temp.Text, "");
+        }
+
+        private void updateStepCount(string triforce)
+        {
+            switch (triforce)
+            {
+                case ("Power"):
+                    pwrCount.Text = TriItems[triforce].Count().ToString();
+                    break;
+                case ("Wisdom"):
+                    wisCount.Text = TriItems[triforce].Count().ToString();
+                    break;
+                case ("Courage"):
+                    crgCount.Text = TriItems[triforce].Count().ToString();
+                    break;
+                default:
+                    pwrCount.Text = "0";
+                    wisCount.Text = "0";
+                    crgCount.Text = "0";
+                    break;
             }
         }
     }
---
+++ ZOOTER Hints/Path.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZOOTR_Hints
+{
+    class Path
+    {
+        public string region { get; set; }
+        public string triforce { get; set; }
+        public List<String> items;
+
+        public Path()
+        {
+            items = new List<String>();
+        }
+
+        public Path(string Region, string Tri)
+        {
+            region = Region;
+            triforce = Tri;
+            items = new List<String>();
+        }
+
+        public Path(string Region, string Tri, List<string> already)
+        {
+            region = Region;
+            triforce = Tri;
+            items = already;
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (obj is Path)
+            {
+                Path temp = (Path)obj;
+                return this.region.Equals(temp.region) && this.triforce.Equals(temp.triforce);
+            }
+            else
+                return false;
+        }
+    }
+}
